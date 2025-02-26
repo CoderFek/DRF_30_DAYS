d63687aa6afff04467a0dcfa4d2fb84144532f4d
CoderFek <praveen.anand2212@gmail.com>
2025-02-26 19:09:33 +0530

    accessing-method-field-without-serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,11 +4,16 @@ from .models import Product
 from django.forms.models import model_to_dict
 import json
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET"])
 def home_view(request):
     model_data = Product.objects.all().order_by("?").first()
 
     if model_data:
-        data = model_to_dict(model_data, fields=['id' ,'title', 'price'])
+        data = model_to_dict(model_data, fields=['id' ,'title', 'price', 'sale_price'])
+        data['sale_price'] = model_data.sale_price
 
-    return HttpResponse(data, headers={"content-type":"application/json"})
+    return Response(data, headers={"content-type":"application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,5 +2,5 @@ import requests
 
 response = requests.get('http://127.0.0.1:8000/')
 
-print(response.headers)
+print(response.text)
 #print(response.json())
